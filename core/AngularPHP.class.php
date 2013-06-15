e3c87aed52eae1946812e5383f90bd8a808570a9
Pedro M. Silva <pemiolsi@hotmail.com>
2013-06-15 18:59:42 +0100

    Small changes.

--- core/AngularPHP.class.php
+++ core/AngularPHP.class.php
@@ -9,7 +9,6 @@ if (!defined('APPRUNNING')){
 require(Config::$dirPath.'core\Decorator.class.php');
 require(Config::$dirPath.'core\DependencyInjection.class.php');
 require(Config::$dirPath.'core\ModulesManager.class.php');
-require(Config::$dirPath.'modules\HelperFunctions.php');
 
 class AngularPHP {
 	
@@ -36,107 +35,10 @@ class AngularPHP {
 		$this->dependencyInjector = new DependencyInjection();
 		
 		//Registers the default AppManager Provider
-		$this->dependencyInjector->registerInjectionProvider('AngularPHP', $this);
+		$this->dependencyInjector->registerInjectionProvider('AngularPHP\AngularPHP', $this);
 		
 		//Loads the ModulesManager class
 		$this->modulesManager = new ModulesManager($this);
 	}
-	
-	//Old Code - nothing works beneath here
-	public function CheckPage(){
-		global $URI, $smarty;
-		
-		$this->pageType = $URI->get_segment(0);
-		if (empty($this->pageType))	{
-			$this->pageType = 'home';
-		}
-		
-		return $this->pageType;
-	}
-	
-	public function buildPage(){
-		global $dir_path, $http_path, $smarty, $Auth, $URI;
-		
-		if (func_num_args() == 0){
-			if (empty($this->pageType)){
-				$this->CheckPage();
-			}
-			$pageType = $this->pageType;
-		} else {
-				$pageType = func_get_arg(0);
-		}
-		
-		$i = 1;
-		
-		while ($i < func_num_args()){
-			$URI->set_segment($i, func_get_arg($i));
-			$i++;
-		}
-		
-		if (!file_exists($dir_path.'pages/'.$pageType.'.page.php')){
-			return(false);
-		}
-		
-		require_once($dir_path.'pages/'.$pageType.'.page.php');
-		
-		$pageReflection = new ReflectionClass($pageType.'_PAGE');
-		if ($pageReflection->isSubclassOf('Page')){
-			$pageType = $pageType.'_PAGE';
-			$page = new $pageType;
-			if ($page->isShowable() === true){				
-				$page->beforeShow();
-				$page->show();
-			} else {
-				header('HTTP/1.1 403 Forbidden');
-			}
-		} else {
-			header('HTTP/1.1 404 Not Found');
-		}
-	}
-	
-	public function checkRequest(){
-		global $URI, $dir_path;
-		
-		if ($URI->get_segment(0) == ''){
-			$this->requestType = 'connect';
-		} else {
-			$this->requestType = $URI->get_segment(0);
-		}
-		
-		return($this->requestType);
-	}
-	
-	public function executeRequest($reqType = ''){
-		global $URI, $dir_path;
-		
-		if ($reqType == ''){
-			if (empty($this->requestType)){
-				if ($this->checkRequest() === false){
-					return(false);
-				}
-			}
-			$reqType = $this->requestType;
-		}
-		
-		if (!file_exists($dir_path.'ajax/'.$reqType.'.ajax.php')){
-			header('HTTP/1.1 404 Not Found');
-		}
-		
-		require_once($dir_path.'ajax/'.$reqType.'.ajax.php');
-		
-		$requestReflection = new ReflectionClass($reqType.'_AJAX');
-		if ($requestReflection->isSubclassOf('AJAXRequest')){
-			$reqType = $reqType.'_AJAX';
-			$request = new $reqType;
-			if ($request->isCallable() === true){
-				return($request->execute());
-			} else {
-				header('HTTP/1.1 403 Forbidden');
-			}
-		} else {
-			header('HTTP/1.1 404 Not Found');
-		}
-	}
-
 }
 ?>
